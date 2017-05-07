df6b6949045f52a735b22bd0ed569cbc27c8f3c7
paul <paul.davidouski@gmail.com>
2017-05-07 19:27:11 +0300

    created BL project, mail sending was started to develop

---
+++ HongGia.BL/Mailer/Mailer.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+using HongGia.Core.Interfaces.Base;
+
+namespace HongGia.BL.Mailer
+{
+	public class Mailer
+	{
+		private string _toMail;
+		private IEmail _email;
+
+		public Mailer(IEmail email)
+		{
+			_email = email;
+			_toMail = "paul.davidovski@gmail.com";
+		}
+
+		public bool Send()
+		{
+			if (_email == null)
+			{
+				return false;
+			}
+
+			//var mail = new MailMessage
+			//{
+			//	From = new MailAddress(_email.Mail)
+			//};
+
+			//mail.To.Add(_toMail);
+			//mail.Subject = _email.Subject;
+			//mail.Body = _email.Message;
+
+			//var smtpServer = new SmtpClient("smtp.gmail.com")
+			//{
+			//	Port = 587,
+			//	Credentials = new System.Net.NetworkCredential(_toMail, "334242343qweASS"),
+			//	EnableSsl = true
+			//};
+
+			//smtpServer.Send(mail);
+
+
+
+			var mail = new MailMessage(_email.Mail, _toMail)
+			{
+				Subject = _email.Subject,
+				Body = _email.Message
+			};
+
+			var smtpServer = new SmtpClient
+			{
+				Port = 25,
+				DeliveryMethod = SmtpDeliveryMethod.Network,
+				UseDefaultCredentials = false,
+				Host = "smtp.google.com"
+			};
+
+			smtpServer.Send(mail);
+
+			return true;
+		}
+	}
+}
---
+++ HongGia.Core/Models/Base/Email.cs
@@ -0,0 +1,12 @@
+﻿using HongGia.Core.Interfaces.Base;
+
+namespace HongGia.Core.Models.Base
+{
+	public class Email : IEmail
+	{
+		public string Name { get; set; }
+		public string Mail { get; set; }
+		public string Subject { get; set; }
+		public string Message { get; set; }
+	}
+}
