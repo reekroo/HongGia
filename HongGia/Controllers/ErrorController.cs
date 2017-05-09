86d1616ee3b894a68959839bc37bef541bb454d6
paul <paul.davidouski@gmail.com>
2017-05-09 17:51:28 +0300

    created error pages

---
+++ HongGia/Controllers/ErrorController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+
+using HongGia.Core.Controllers;
+
+namespace HongGia.Controllers
+{
+	public class ErrorController : DefaultController
+	{
+		//public ActionResult Index()
+		//{
+		//	Response.StatusCode = 404;
+		//	return View("NotFound");
+		//}
+
+		public ActionResult NotFound()
+		{
+			Response.StatusCode = 404;
+			return View();
+		}
+
+		public ActionResult Forbidden()
+		{
+			Response.StatusCode = 403;
+			return View();
+		}
+
+		public ActionResult BadRequest()
+		{
+			Response.StatusCode = 400;
+			return View();
+		}
+
+		public ActionResult Server()
+		{
+			Response.StatusCode = 500;
+			return View();
+		}
+	}
+}
