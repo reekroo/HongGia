299233c20eb2a8b714f08ba4f4c5844bde840809
paul <paul.davidouski@gmail.com>
2017-05-09 17:51:13 +0300

    created defualt route mapping

--- HongGia/App_Start/RouteConfig.cs
+++ HongGia/App_Start/RouteConfig.cs
@@ -3,24 +3,25 @@ using System.Web.Routing;
 
 namespace HongGia
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "lang",
-                url: "{lang}/{controller}/{action}/{id}",
-                defaults: new { lang = "ru", controller = "Home", action = "Index", id = UrlParameter.Optional },
-                constraints: new { lang = @"ru|en|de|fr|lt|sv" }
-            );
+			routes.MapRoute(
+				name: "lang",
+				url: "{lang}/{controller}/{action}/{id}",
+				defaults: new { lang = "ru", controller = "Home", action = "Index", id = UrlParameter.Optional },
+				constraints: new { lang = @"ru|en|de|fr|lt|sv" }
+			);
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
-        }
-    }
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { lang = "ru", controller = "Home", action = "Index", id = UrlParameter.Optional },
+				constraints: new { lang = @"ru|en|de|fr|lt|sv" }
+			);
+		}
+	}
 }
